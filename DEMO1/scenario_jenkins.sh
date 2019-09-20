97ef283190e5202c808237c09b0c4a34f8cee3e4
Yaroslav Ryzhan <slavik.ryzhan@gmail.com>
2019-09-20 12:33:05 +0300

    Add jenkinslile

--- DEMO1/scenario_jenkins.sh
+++ DEMO1/scenario_jenkins.sh
@@ -7,7 +7,7 @@ sudo yum install java-1.8.0-openjdk -y
 
 sudo yum install maven -y 
 sudo sh -c 'cat << EOF >> /etc/profile.d/maven.sh
-export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk-1.8.0.222.b10-0.el7_6.x86_64
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-1.el7_7.x86_64/jre/
 export M2_HOME=/usr/share/maven/
 export MAVEN_HOME=/usr/share/maven/
 export PATH=${M2_HOME}/bin:${PATH}
