e0713384f2f83ff779d2f9ae5f7b158e0abae33a
Yaroslav Ryzhan <slavik.ryzhan@gmail.com>
2019-10-15 14:11:13 +0300

    Postgres 9.4 fix

--- Task2/scenario_db.sh
+++ Task2/scenario_db.sh
@@ -16,13 +16,11 @@ yum install postgresql94-server postgresql94-contrib postgresql94-libs postgresq
 systemctl start postgresql-9.4.service
 systemctl enable postgresql-9.4.service
 echo "<<<<<<<<<<<<<<<<<< Create DB >>>>>>>>>>>>>>>>>>>>"
-su postgres <<_EOM_
-psql <<_EOF_
+psql -U postgres <<_EOF_
 CREATE USER moodle_devops WITH PASSWORD '1qaz2wsx';
 CREATE DATABASE moodle WITH OWNER moodle_devops ENCODING 'UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' TEMPLATE=template0;
 _EOF_
-exit
-_EOM_
+
 
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '192.168.56.20'/" /var/lib/pgsql/9.4/data/postgresql.conf
 echo "host all all 192.168.56.10/32 md5" >> /var/lib/pgsql/9.4/data/pg_hba.conf
